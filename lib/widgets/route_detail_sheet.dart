1be6be2aa89c935aed20dc73feaea0cbba0ec5f7
Jean-Luc Herbst <eleven300011@gmail.com>
2022-01-28 18:07:05 +0100

    test

--- lib/widgets/route_detail_sheet.dart
+++ lib/widgets/route_detail_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:lanes/models/routeModels.dart';
-import 'package:lanes/style/style.dart';
 
 class RouteDetailSheet extends StatelessWidget {
   RouteDetailSheet({Key? key, required this.routeObj}) : super(key: key);
